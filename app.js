3dd728a251ec44f97a08a0d543af489ddef64e97
Karankumar Patel <200350059@student.georgianc.on.ca>
2019-05-29 18:52:01 -0400

    heroku fix

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hey There!');
+    res.send(`Table Of Content \r\n /about - About myself \r\n /contact - That's how you can contact me.` );
 });
 
 app.get('/about', (req, res) => {
-    res.send('Hey! Its me, Karan Patel');
+    res.send(`Hello! My name is Karankumar Patel. I am a mechanical engineer and a computer programmer as well.`);
 });
 
 app.get('/contact', (req, res) => {
-    res.send('You can call me anytime you want.');
+    res.send(`You can call send me mail on 200350059@student.georgianc.on.ca or call me anytime on +17058960219.`);
 });
 
 const port = process.env.PORT || 2000;
